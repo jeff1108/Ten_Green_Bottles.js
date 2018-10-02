f53dd352b9f44461c0f43c0e292a087e7b8599a4
Hung Ka Kin <hungkakin@Hungs-MacBook-Pro.local>
2018-10-02 22:05:22 +0800

    Add numberTowords function

--- spec/TenGreenBottleSpec.js
+++ spec/TenGreenBottleSpec.js
@@ -1,6 +1,6 @@
 describe("TenGreenBottle", () => {
   it("returns ten green bottle", () => {
-    expect(TenGreenBottle()).toEqual("Ten green bottles hanging on the wall\n" +
+    expect(TenGreenBottle(10)).toEqual("Ten green bottles hanging on the wall\n" +
                                      "And if one green bottle should accidentally fall\n" +
                                      "There'll be nine green bottles hanging on the wall")
   });
--- src/TenGreenBottle.js
+++ src/TenGreenBottle.js
@@ -1,6 +1,11 @@
-function TenGreenBottle() {
-  firstline = "Ten green bottles hanging on the wall\n"
+function TenGreenBottle(num) {
+  firstline = `${numberToWords.toWords(num).capitalize()} green bottles hanging on the wall\n`
   secondline = "And if one green bottle should accidentally fall\n"
-  lastline = "There'll be nine green bottles hanging on the wall"
+  lastline = `There'll be ${numberToWords.toWords(num - 1)} green bottles hanging on the wall`
   return firstline + secondline + lastline
+}
+
+
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
 }
