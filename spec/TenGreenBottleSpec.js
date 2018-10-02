4ce1567391a1d77a7bef6ee3f39b4c0b4a5ee276
Hung Ka Kin <hungkakin@Hungs-MacBook-Pro.local>
2018-10-02 23:05:31 +0800

    Refactor the accommodate plural words for different verses

--- spec/TenGreenBottleSpec.js
+++ spec/TenGreenBottleSpec.js
@@ -2,20 +2,13 @@ describe("TenGreenBottle", () => {
   var song
 
   beforeEach(function () {
-    song = new TenGreenBottle()
+    song = new TenGreenBottle(10)
   });
 
   it("returns ten green bottle", () => {
-    expect(song.sing(10)).toEqual("Ten green bottles hanging on the wall\n" +
+    expect(song.verseThreeOnwardLyrics(10)).toEqual("Ten green bottles hanging on the wall\n" +
                                   "Ten green bottles hanging on the wall\n" +
                                   "And if one green bottle should accidentally fall\n" +
                                   "There'll be nine green bottles hanging on the wall")
   });
-
-  // it("returns one green bottle", () => {
-  //   expect(song.sing(1)).toEqual("One green bottle hanging on the wall\n" +
-  //                                     "One green bottle hanging on the wall\n" +
-  //                                     "And if one green bottle should accidentally fall\n" +
-  //                                     "There'll be zero green bottles hanging on the wall")
-  // });
 });
--- src/TenGreenBottle.js
+++ src/TenGreenBottle.js
@@ -1,13 +1,26 @@
 (function(exports) {
   function TenGreenBottle(num) {
+    this.num = num;
+    // this.counter = counter;
 }
 
-  TenGreenBottle.prototype.sing = function(num) {
-    firstline = `${numberToWords.toWords(num).capitalize()} green bottles hanging on the wall\n`
-    secondline = "And if one green bottle should accidentally fall\n"
-    lastline = `There'll be ${numberToWords.toWords(num - 1)} green bottles hanging on the wall`
-    return firstline + firstline + secondline + lastline
+  TenGreenBottle.prototype.verseThreeOnwardLyrics = function(num) {
+    return _firstSecondLinePlural(this.num) + _thirdLinePlural() + _lastLinePlural(this.num)
   }
+
+  function _firstSecondLinePlural(num) {
+    var line = `${numberToWords.toWords(num).capitalize()} green bottles hanging on the wall\n`
+    return [line, line].join('')
+  }
+
+  function _thirdLinePlural() {
+    return "And if one green bottle should accidentally fall\n"
+  }
+
+  function _lastLinePlural(num) {
+    return `There'll be ${numberToWords.toWords(num - 1)} green bottles hanging on the wall`
+  }
+
 
   // function last_lyrics(num) {
   //   firstline = `${numberToWords.toWords(num).capitalize()} green bottle hanging on the wall\n`
