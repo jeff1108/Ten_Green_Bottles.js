1b550ae6ff1c75dd3a4701c65d10ae8898bbe899
Hung Ka Kin <hungkakin@Hungs-MacBook-Pro.local>
2018-10-02 22:36:24 +0800

    Introduce object oriented

--- spec/TenGreenBottleSpec.js
+++ spec/TenGreenBottleSpec.js
@@ -1,7 +1,21 @@
 describe("TenGreenBottle", () => {
-  it("returns ten green bottle", () => {
-    expect(TenGreenBottle(10)).toEqual("Ten green bottles hanging on the wall\n" +
-                                     "And if one green bottle should accidentally fall\n" +
-                                     "There'll be nine green bottles hanging on the wall")
+  var song
+
+  beforeEach(function () {
+    song = new TenGreenBottle()
   });
+
+  it("returns ten green bottle", () => {
+    expect(song.sing(10)).toEqual("Ten green bottles hanging on the wall\n" +
+                                  "Ten green bottles hanging on the wall\n" +
+                                  "And if one green bottle should accidentally fall\n" +
+                                  "There'll be nine green bottles hanging on the wall")
+  });
+
+  // it("returns one green bottle", () => {
+  //   expect(song.sing(1)).toEqual("One green bottle hanging on the wall\n" +
+  //                                     "One green bottle hanging on the wall\n" +
+  //                                     "And if one green bottle should accidentally fall\n" +
+  //                                     "There'll be zero green bottles hanging on the wall")
+  // });
 });
--- src/TenGreenBottle.js
+++ src/TenGreenBottle.js
@@ -1,11 +1,25 @@
-function TenGreenBottle(num) {
-  firstline = `${numberToWords.toWords(num).capitalize()} green bottles hanging on the wall\n`
-  secondline = "And if one green bottle should accidentally fall\n"
-  lastline = `There'll be ${numberToWords.toWords(num - 1)} green bottles hanging on the wall`
-  return firstline + secondline + lastline
+(function(exports) {
+  function TenGreenBottle(num) {
 }
 
+  TenGreenBottle.prototype.sing = function(num) {
+    firstline = `${numberToWords.toWords(num).capitalize()} green bottles hanging on the wall\n`
+    secondline = "And if one green bottle should accidentally fall\n"
+    lastline = `There'll be ${numberToWords.toWords(num - 1)} green bottles hanging on the wall`
+    return firstline + firstline + secondline + lastline
+  }
 
-String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
-}
+  // function last_lyrics(num) {
+  //   firstline = `${numberToWords.toWords(num).capitalize()} green bottle hanging on the wall\n`
+  //   secondline = `${numberToWords.toWords(num).capitalize()} green bottle hanging on the wall\n`
+  //   thirdline = "And if one green bottle should accidentally fall\n"
+  //   lastline = `There'll be ${numberToWords.toWords(num - 1)} green bottle hanging on the wall`
+  //   return firstline + secondline + lastline
+  // }
+  //
+  String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  }
+
+  exports.TenGreenBottle = TenGreenBottle;
+})(this);
